bb8c49ddbeffa38fac5be6470b1e4b8501525bf2
eden <eden@sfc.wide.ad.jp>
2015-07-19 13:18:58 +0900

    add arp_{generate/learn}

---
+++ examples/forward/arp.c
@@ -0,0 +1,130 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <linux/if.h>
+#include <linux/if_packet.h>
+#include <linux/if_ether.h>
+#include <linux/if_arp.h>
+
+int arp_generate(void *buf, int buf_len, uint16_t opcode,
+	uint8_t *dest_mac, uint8_t *src_mac,
+	uint32_t dest_ip, uint32_t src_ip)
+{
+	struct ethhdr *eth;
+	struct arphdr *arp;
+	int len = 0;
+
+	eth = (struct ethhdr *)buf;
+	len += sizeof(struct ethhdr);
+	if(len > buf_len)
+		goto err_buf_size;
+
+	memset(eth->h_dest, 0xFF, ETH_ALEN);
+	memcpy(eth->h_source, src_mac, ETH_ALEN);
+	eth->h_proto = htons(ETH_P_ARP);
+
+	arp = (struct arphdr *)(buf + len);
+	len += sizeof(struct arphdr);
+	if(len > buf_len)
+		goto err_buf_size;
+
+	arp->ar_hrd	= htons(ARPHRD_ETHER);
+	arp->ar_pro	= htons(ETH_P_IP);
+	arp->ar_hln	= ETH_ALEN;
+	arp->ar_pln	= 4;
+	arp->ar_op	= htons(ARPOP_REQUEST);
+
+	if(len + 8 + ETH_ALEN * 2 > buf_len){
+		goto err_buf_size;
+	}
+
+	memcpy(buf + len, src_mac, ETH_ALEN);
+	len += ETH_ALEN;
+
+	memcpy(buf + len, &src_ip, 4);
+	len += 4;
+
+	if(opcode == ARPOP_REQUEST){
+		memset(buf + len, 0, ETH_ALEN);
+	}else if(opcode == ARPOP_REPLY){
+		memcpy(buf + len, dest_mac, ETH_ALEN);
+	}else{
+		goto err_unsupp_opcode;
+	}
+	len += ETH_ALEN;
+
+	memcpy(buf + len, &dest_ip, 4);
+	len += 4;
+
+	return len;
+
+err_unsupp_opcode:
+err_buf_size:
+	return -1;
+}
+
+int arp_learn(void *buf, int buf_len,
+	uint8_t *src_mac, uint32_t src_ip)
+{
+	struct ethhdr *eth;
+	struct arphdr *arp;
+	uint8_t arp_dest_mac[6];
+	uint8_t arp_src_mac[6];
+	uint32_t arp_dest_ip;
+	uint32_t arp_src_ip;
+	char addrstr[256];
+	int len = 0;
+
+	eth = (struct ethhdr *)buf;
+	len += sizeof(struct ethhdr);
+	if(len > buf_len)
+		goto err_buf_size;
+
+	arp = (struct arphdr *)(buf + len);
+	len += sizeof(struct arphdr);
+	if(len > buf_len)
+		goto err_buf_size;
+
+	if(len + 8 + ETH_ALEN * 2 > buf_len)
+		goto err_buf_size;
+
+	memcpy(arp_src_mac, buf + len, ETH_ALEN);
+	len += ETH_ALEN;
+
+	memcpy(&arp_src_ip, buf + len, 4);
+	len += 4;
+
+	memcpy(arp_dest_mac, buf + len, ETH_ALEN);
+	len += ETH_ALEN;
+
+	memcpy(&arp_dest_ip, buf + len, 4);
+	len += 4;
+
+	// learn here
+	inet_ntop(AF_INET, &arp_src_ip, addrstr, sizeof(addrstr));
+	printf("learned ip = %s, mac = %02x:%02x:%02x:%02x:%02x:%02x\n",
+		addrstr, arp_src_mac[0], arp_src_mac[1], arp_src_mac[2],
+		arp_src_mac[3], arp_src_mac[4], arp_src_mac[5]);
+
+	if(arp->ar_op == htons(ARPOP_REQUEST)){
+		if(!memcmp(&src_ip, &arp_dest_ip, 4)){
+			len = arp_generate(buf, 2048, ARPOP_REPLY,
+				arp_src_mac, src_mac, arp_dest_ip, src_ip);
+			return len;
+		}
+	}
+
+	return 0;
+
+err_buf_size:
+	return -1;
+}
+
+int arp_lookup()
+{
+
+}
+
---
+++ examples/forward/fib.c
@@ -0,0 +1,20 @@
+
+int fib_import()
+{
+
+}
+
+int fib_route_update()
+{
+
+}
+
+int fib_route_delete()
+{
+
+}
+
+int fib_lookup()
+{
+
+}
