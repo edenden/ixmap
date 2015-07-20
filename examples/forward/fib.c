7a8bee1e185593ee59f17ca491eab769024e3f97
eden <eden@sfc.wide.ad.jp>
2015-07-20 17:44:29 +0900

    renamed forward.c to thread.c

--- examples/forward/fib.c
+++ examples/forward/fib.c
@@ -1,18 +1,18 @@
 
-int fib_import(struct fib *fib, char *command)
+int ixmapfwd_fib_import(struct fib *fib, char *command)
 {
 	int opcode, family;
 	uint32_t prefix[4], nexthop[4];
 	unsigned int prefix_len;
 
 	switch(opcode){
-		case FIB_ADD:
-			fib_route_update(fib, family, prefix, prefix_len, nexthop);
-			break;
-		case FIB_DELETE:
-			fib_route_delete(fib, family, prefix, prefix_len);
-			break;
-		default:
+	case FIB_ADD:
+		fib_route_update(fib, family, prefix, prefix_len, nexthop);
+		break;
+	case FIB_DELETE:
+		fib_route_delete(fib, family, prefix, prefix_len);
+		break;
+	default:
 	}
 
 
@@ -20,22 +20,22 @@ int fib_import(struct fib *fib, char *command)
 
 }
 
-int fib_route_update(struct fib *fib, int family,
+int ixmapfwd_fib_route_update(struct fib *fib, int family,
 	uint32_t *prefix, unsigned int prefix_len, uint32_t *nexthop)
 {
 	struct fib_entry entry;
 	unsigned int port_index, family_len;
 
 	switch(family){
-		case AF_INET:
-			family_len = 32;
-			break;
-		case AF_INET6:
-			family_len = 128;
-			break;
-		default:
-			goto err_invalid_family;
-			break;
+	case AF_INET:
+		family_len = 32;
+		break;
+	case AF_INET6:
+		family_len = 128;
+		break;
+	default:
+		goto err_invalid_family;
+		break;
 	}
 
 	// TBD: Get the port index here
@@ -54,21 +54,21 @@ err_invalid_family:
 	return -1;
 }
 
-int fib_route_delete(struct fib *fib, int family,
+int ixmapfwd_fib_route_delete(struct fib *fib, int family,
 	uint32_t *prefix, unsigned int prefix_len)
 {
 	unsigned int family_len;
 
 	switch(family){
-		case AF_INET:
-			family_len = 32;
-			break;
-		case AF_INET6:
-			family_len = 128;
-			break;
-		default:
-			goto err_invalid_family;
-			break;
+	case AF_INET:
+		family_len = 32;
+		break;
+	case AF_INET6:
+		family_len = 128;
+		break;
+	default:
+		goto err_invalid_family;
+		break;
 	}
 
 	return trie_delete(fib->trie_root, family_len, prefix, prefix_len);
@@ -77,21 +77,21 @@ err_invalid_family:
 	return -1;
 }
 
-struct fib_entry *fib_lookup(struct fib *fib, int family,
+struct ixmapfwd_fib_entry *fib_lookup(struct fib *fib, int family,
 	uint32_t *destination)
 {
 	unsigned int family_len;
 
 	switch(family){
-		case AF_INET:
-			family_len = 32;
-			break;
-		case AF_INET6:
-			family_len = 128;
-			break;
-		default:
-			goto err_invalid_family;
-			break;
+	case AF_INET:
+		family_len = 32;
+		break;
+	case AF_INET6:
+		family_len = 128;
+		break;
+	default:
+		goto err_invalid_family;
+		break;
 	}
 
 	return trie_lookup(fib->trie_root, family_len, destination);
