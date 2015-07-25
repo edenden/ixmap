b47c9f907be59393cb3f75db3b9b0b5e4bd53b1e
eden <eden@sfc.wide.ad.jp>
2015-07-25 18:55:44 +0900

    fix arp_generate()

--- examples/forward/arp.c
+++ examples/forward/arp.c
@@ -10,11 +10,15 @@
 #include <linux/if_arp.h>
 
 int arp_generate(uint16_t opcode, void *buf, int buf_len,
-	uint8_t *dest_mac, uint8_t *src_mac,
-	uint32_t *dest_ip, uint32_t *src_ip)
+	uint8_t *dst_mac, uint8_t *src_mac,
+	uint32_t *dst_ip, uint32_t *src_ip)
 {
 	struct ethhdr *eth;
 	struct arphdr *arp;
+	uint8_t *arp_dst_mac;
+	uint8_t *arp_src_mac;
+	uint32_t *arp_dst_ip;
+	uint32_t *arp_src_ip;
 	int len = 0;
 
 	eth = (struct ethhdr *)buf;
@@ -22,42 +26,50 @@ int arp_generate(uint16_t opcode, void *buf, int buf_len,
 	if(len > buf_len)
 		goto err_buf_size;
 
-	memset(eth->h_dest, 0xFF, ETH_ALEN);
-	memcpy(eth->h_source, src_mac, ETH_ALEN);
-	eth->h_proto = htons(ETH_P_ARP);
-
 	arp = (struct arphdr *)(buf + len);
 	len += sizeof(struct arphdr);
 	if(len > buf_len)
 		goto err_buf_size;
 
-	arp->ar_hrd	= htons(ARPHRD_ETHER);
-	arp->ar_pro	= htons(ETH_P_IP);
-	arp->ar_hln	= ETH_ALEN;
-	arp->ar_pln	= 4;
-	arp->ar_op	= htons(opcode);
-
 	if(len + 8 + ETH_ALEN * 2 > buf_len){
 		goto err_buf_size;
 	}
 
-	memcpy(buf + len, src_mac, ETH_ALEN);
+	arp_src_mac = (uint8_t *)(buf + len);
 	len += ETH_ALEN;
 
-	memcpy(buf + len, src_ip, 4);
+	arp_src_ip = (uint32_t *)(buf + len);
 	len += 4;
 
+	arp_dst_mac = (uint8_t *)(buf + len);
+	len += ETH_ALEN;
+
+	arp_dst_ip = (uint32_t *)(buf + len);
+	len += 4;
+
+	/* ARP payload setup */
+	memcpy(arp_src_mac, src_mac, ETH_ALEN);
+	memcpy(arp_src_ip, src_ip, 4);
 	if(opcode == ARPOP_REQUEST){
-		memset(buf + len, 0, ETH_ALEN);
+		memset(arp_dst_mac, 0, ETH_ALEN);
 	}else if(opcode == ARPOP_REPLY){
-		memcpy(buf + len, dest_mac, ETH_ALEN);
+		memcpy(arp_dst_mac, dst_mac, ETH_ALEN);
 	}else{
 		goto err_unsupp_opcode;
 	}
-	len += ETH_ALEN;
+	memcpy(arp_dst_ip, dest_ip, 4);
 
-	memcpy(buf + len, dest_ip, 4);
-	len += 4;
+	/* ARP header setup */
+	arp->ar_hrd     = htons(ARPHRD_ETHER);
+	arp->ar_pro     = htons(ETH_P_IP);
+	arp->ar_hln     = ETH_ALEN;
+	arp->ar_pln     = 4;
+	arp->ar_op      = htons(opcode);
+
+	/* ETHER header setup */
+	memset(eth->h_dest, 0xFF, ETH_ALEN);
+	memcpy(eth->h_source, src_mac, ETH_ALEN);
+	eth->h_proto = htons(ETH_P_ARP);
 
 	return len;
 
