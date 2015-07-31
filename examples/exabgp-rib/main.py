542a0b37a4c436d208772958cf82a730b0cc7556
eden <eden@sfc.wide.ad.jp>
2015-07-31 10:35:17 +0900

    add main if

--- examples/exabgp-rib/main.py
+++ examples/exabgp-rib/main.py
@@ -6,26 +6,27 @@ import json
 import bgp
 import rib
 
-rib = rib.Rib()
-bgp = bgp.Bgp()
+if __name__ == "__main__":
+	rib = rib.Rib()
+	bgp = bgp.Bgp()
 
-route = {}
-route["protocol"]	= "connected"
-route["protocol-id"]	= 0
-route["id"]		= "connected-1"
-rib.update("203.178.138.64/27", route)
+	route = {}
+	route["protocol"]	= "connected"
+	route["protocol-id"]	= 0
+	route["id"]		= "connected-1"
+	rib.update("203.178.138.64/27", route)
 
-#route = {}
-#route["protocol"]	= "static"
-#route["protocol-id"]	= 1
-#route["id"]		= "static-1"
-#route["nexthop"]	= "203.178.138.65"
-#rib.update("0.0.0.0/0", route)
+	#route = {}
+	#route["protocol"]	= "static"
+	#route["protocol-id"]	= 1
+	#route["id"]		= "static-1"
+	#route["nexthop"]	= "203.178.138.65"
+	#rib.update("0.0.0.0/0", route)
 
-for line in sys.stdin:
-	data = json.loads(line)
-	if data["type"] == "state":
-		bgp.nbr_change(rib, data)
-	elif data["type"] == "update":
-		bgp.route_change(rib, data)
+	for line in sys.stdin:
+		data = json.loads(line)
+		if data["type"] == "state":
+			bgp.nbr_change(rib, data)
+		elif data["type"] == "update":
+			bgp.route_change(rib, data)
 
