3eddd18a7b24de5e6cfe77352b9c64ce03e091c5
eden <eden@sfc.wide.ad.jp>
2015-08-15 00:40:09 +0900

    prefetch received buffer

--- lib/driver.c
+++ lib/driver.c
@@ -86,6 +86,7 @@ void ixmap_rx_assign(struct ixmap_plane *plane, unsigned int port_index,
 {
 	struct ixmap_port *port;
 	struct ixmap_ring *rx_ring;
+	union ixmap_adv_rx_desc *rx_desc;
 	unsigned int total_allocated;
 	uint16_t max_allocation;
 
@@ -98,12 +99,9 @@ void ixmap_rx_assign(struct ixmap_plane *plane, unsigned int port_index,
 
 	total_allocated = 0;
 	while(likely(total_allocated < max_allocation)){
-		union ixmap_adv_rx_desc *rx_desc;
 		uint16_t next_to_use;
 		uint64_t addr_dma;
 		int slot_index;
-
-		rx_desc = IXGBE_RX_DESC(rx_ring, rx_ring->next_to_use);
 
 		slot_index = ixmap_slot_assign(buf);
 		if(slot_index < 0){
@@ -115,6 +113,8 @@ void ixmap_rx_assign(struct ixmap_plane *plane, unsigned int port_index,
 		ixmap_slot_attach(rx_ring, rx_ring->next_to_use, slot_index);
 		addr_dma = (uint64_t)ixmap_slot_addr_dma(buf,
 				slot_index, port_index);
+
+		rx_desc = IXGBE_RX_DESC(rx_ring, rx_ring->next_to_use);
 
 		rx_desc->read.pkt_addr = htole64(addr_dma);
 		rx_desc->read.hdr_addr = 0;
@@ -162,8 +162,6 @@ void ixmap_tx_assign(struct ixmap_plane *plane, unsigned int port_index,
 		return;
 	}
 
-	tx_desc = IXGBE_TX_DESC(tx_ring, tx_ring->next_to_use);
-
 	if(unlikely(slot_size > IXGBE_MAX_DATA_PER_TXD)){
 		port->count_tx_xmit_failed++;
 		ixmap_slot_release(buf, slot_index);
@@ -176,6 +174,7 @@ void ixmap_tx_assign(struct ixmap_plane *plane, unsigned int port_index,
 		(void *)addr_dma, size);
 
 	/* set type for advanced descriptor with frame checksum insertion */
+	tx_desc = IXGBE_TX_DESC(tx_ring, tx_ring->next_to_use);
 	tx_flags = IXGBE_ADVTXD_DTYP_DATA | IXGBE_ADVTXD_DCMD_DEXT
 		| IXGBE_ADVTXD_DCMD_IFCS;
 	cmd_type = slot_size | IXGBE_TXD_CMD_EOP | IXGBE_TXD_CMD_RS | tx_flags;
@@ -224,6 +223,7 @@ void ixmap_rx_clean(struct ixmap_plane *plane, unsigned int port_index,
 {
 	struct ixmap_port *port;
 	struct ixmap_ring *rx_ring;
+	union ixmap_adv_rx_desc *rx_desc;
 	unsigned int total_rx_packets;
 
 	port = &plane->ports[port_index];
@@ -231,10 +231,10 @@ void ixmap_rx_clean(struct ixmap_plane *plane, unsigned int port_index,
 
 	total_rx_packets = 0;
 	while(likely(total_rx_packets < port->rx_budget)){
-		union ixmap_adv_rx_desc *rx_desc;
 		uint16_t next_to_clean;
 		int slot_index;
 		unsigned int slot_size;
+		void *slot_buf;
 
 		if(unlikely(rx_ring->next_to_clean == rx_ring->next_to_use)){
 			break;
@@ -267,16 +267,17 @@ void ixmap_rx_clean(struct ixmap_plane *plane, unsigned int port_index,
 		/* retrieve a buffer address from the ring */
 		slot_index = ixmap_slot_detach(rx_ring, rx_ring->next_to_clean);
 		slot_size = le16toh(rx_desc->wb.upper.length);
-		ixmap_print("Rx: packet received size = %d\n", slot_size);
 		process(slot_index, slot_size, port_index, opaque);
 
-		/* XXX: Should we prefetch the packet buffer ? */
+		ixmap_print("Rx: packet received size = %d\n", slot_size);
 
 		next_to_clean = rx_ring->next_to_clean + 1;
 		rx_ring->next_to_clean = 
 			(next_to_clean < port->num_rx_desc) ? next_to_clean : 0;
 
-		/* XXX: Should we prefetch the next_to_clean desc ? */
+		slot_index = ixmap_slot_detach(rx_ring, rx_ring->next_to_clean);
+		slot_buf = ixmap_slot_addr_virt(buf, slot_index);
+		prefetchw(slot_buf);
 
 		total_rx_packets++;
 	}
@@ -290,6 +291,7 @@ void ixmap_tx_clean(struct ixmap_plane *plane, unsigned int port_index,
 {
 	struct ixmap_port *port;
 	struct ixmap_ring *tx_ring;
+	union ixmap_adv_tx_desc *tx_desc;
 	unsigned int total_tx_packets;
 
 	port = &plane->ports[port_index];
@@ -297,7 +299,6 @@ void ixmap_tx_clean(struct ixmap_plane *plane, unsigned int port_index,
 
 	total_tx_packets = 0;
 	while(likely(total_tx_packets < port->tx_budget)){
-		union ixmap_adv_tx_desc *tx_desc;
 		uint16_t next_to_clean;
 		int slot_index;
 
--- lib/driver.h
+++ lib/driver.h
@@ -37,6 +37,9 @@
 #define likely(x)       __builtin_expect(!!(x), 1)
 #define unlikely(x)     __builtin_expect(!!(x), 0)
 
+#define prefetch(x)	__builtin_prefetch(x, 0)
+#define prefetchw(x)	__builtin_prefetch(x, 1)
+
 #if defined(__x86_64__) || defined(__i386__) || defined(__amd64__)
 #define mb()  asm volatile("mfence" ::: "memory")
 #define wmb() asm volatile("sfence" ::: "memory")
